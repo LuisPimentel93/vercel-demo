748a356d348a3c3b4c893bc7c0f0f249985ad20d
Luis Pimentel <luispimentel@Luiss-MBP.lan>
2023-05-21 12:53:58 -0400

    testing vercel get

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ require('dotenv').config()
 
 const personController = require('./controllers/Person')
 
-const app = express()
 
-//middlewares
+const app = express()
+// middleware
 app.use(express.json())
 
-//routes
+
 app.use('/people', personController)
 
 // db connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB connected'))
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
 
-const PORT = process.env.PORT
+    const PORT = process.env.PORT
 
-app.listen(PORT, console.log(`listening on PORT ${PORT}`))
+    app.listen(PORT, console.log(`listening on port ${PORT}`))
 
 module.exports = app
